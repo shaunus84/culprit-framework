e004866f5b3253beba4e6178ed7acc9a9ac7e993
Shaun Mitchell <shaunmitchell84@googlemail.com>
2024-07-06 11:56:28 +0100

    update signals

--- culprit-framework/include/culprit-framework/Signals.h
+++ culprit-framework/include/culprit-framework/Signals.h
@@ -7,6 +7,9 @@ namespace framework {
 
 class EnterContextSignal : public EmptySignal {};
 class ExitContextSignal : public EmptySignal {};
+class PreUpdateContextSignal : public EmptySignal {};
+class UpdateContextSignal : public EmptySignal {};
+class PostUpdateContextSignal : public EmptySignal {};
 
 }  // namespace framework
 }  // namespace culprit
